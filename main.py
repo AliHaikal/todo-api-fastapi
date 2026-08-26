b212a939e332b2de164602a79d0ecbaa9915e737
Ali Abdullah <haikalboy73@gmail.com>
2026-08-26 11:31:43 +0500

    Stage 4: explored SQLite

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class TaskCreate(BaseModel):
     title: str
 
 class Task(SQLModel, table=True):
+    __tablename__ = "tasks"
     id: int | None = Field(default=None, primary_key=True)
     title: str
     done: bool = False
